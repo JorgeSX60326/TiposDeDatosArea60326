3c4a01edc5e8ba687feb8085c87ca3fd759f6cc9
Jun <Jun@deivydgeorge>
2021-03-01 20:36:10 -0600

    Commit completo

---
+++ src/tiposdedatosarea60326/TiposDeDatosArea60326.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiposdedatosarea60326;
+
+/**
+ *
+ * @author Jun
+ */
+public class TiposDeDatosArea60326 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    //Declaramos los valores que va a tener nuestro rectangulo.
+    int base = 17;
+    int altura = 8;
+    //Para calcular es area, la formula es, base por altura (bxh), entonces:
+    int area = (base * altura);
+    //Imprimimos el resultado
+    System.out.println("El area del rectangulo es de: " + area);
+    
+    
+    }
+    
+}
